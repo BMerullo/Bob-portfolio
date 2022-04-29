09af4db9fab87fc60bca9111a9592141b971dd78
Bob Merullo <bmerullo85@gmail.com>
2022-04-28 22:36:10 -0600

    fixed dropdown links

--- client/src/components/Dropdown.jsx
+++ client/src/components/Dropdown.jsx
@@ -5,13 +5,17 @@ const Dropdown = () => {
 
   return (
     <div className="dropdown">
-      <div className="dropdown-btn" onClick={(e) => setIsActive(!isActive)}>
+      {/* <div className="dropdown-btn" onClick={(e) => setIsActive(!isActive)}>
         Choose one
-      </div>
+      </div> */}
       {isActive && (
         <div className="dropdown-content">
-          <div className="dropdown-item">linkedin</div>
-          <div className="dropdown-item">github</div>
+          <div className="dropdown-item">
+            <a href="https://www.linkedin.com/in/bobmerullo/">linkedin</a>
+          </div>
+          <div className="dropdown-item">
+            <a href="https://github.com/BMerullo">github</a>
+          </div>
           <div className="dropdown-item">email</div>
         </div>
       )}
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -11,23 +11,58 @@ const Projects = (props) => {
             <h5 className="eyebrow">A collection of my work</h5>
           </div>
           <div className="project-container">
-            <div className="test">
-              <h1>Test Div</h1>
+            <div className="project-card">
+              <div className="project-img-container" id="starwars"></div>
+              <h3 style={cardStyle}>Starwars API</h3>
+              <a
+                className="project-link"
+                href="https://github.com/BMerullo/star-wars-api"
+                style={cardStyle}
+              >
+                source code
+              </a>
             </div>
-            <div className="test">
-              <h1>Test Div</h1>
+            <div className="project-card">
+              <div className="project-img-container"></div>
+              <h3 style={cardStyle}>Project title</h3>
+              <a href="#" style={cardStyle}>
+                source code
+              </a>
             </div>
-            <div className="test">
-              <h1>Test Div</h1>
+            <div className="project-card">
+              <div className="project-img-container"></div>
+              <h3 style={cardStyle}>Project title</h3>
+              <a href="#" style={cardStyle}>
+                source code
+              </a>
             </div>
-            <div className="test">
-              <h1>Test Div</h1>
+            <div className="project-card">
+              <div className="project-img-container"></div>
+              <h3 style={cardStyle}>Project title</h3>
+              <a href="#" style={cardStyle}>
+                source code
+              </a>
             </div>
-            <div className="test">
-              <h1>Test Div</h1>
+            <div className="project-card">
+              <div className="project-img-container"></div>
+              <h3 style={cardStyle}>Project title</h3>
+              <a href="#" style={cardStyle}>
+                source code
+              </a>
             </div>
-            <div className="test">
-              <h1>Test Div</h1>
+            <div className="project-card">
+              <div className="project-img-container"></div>
+              <h3 style={cardStyle}>Project title</h3>
+              <a href="#" style={cardStyle}>
+                source code
+              </a>
+            </div>
+            <div className="project-card">
+              <div className="project-img-container"></div>
+              <h3 style={cardStyle}>Project title</h3>
+              <a href="#" style={cardStyle}>
+                source code
+              </a>
             </div>
           </div>
         </div>
@@ -37,3 +72,7 @@ const Projects = (props) => {
 }
 
 export default Projects
+
+const cardStyle = {
+  padding: '15px',
+}
