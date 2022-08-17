9c6635b5a751b74cc28f43d02e5bac8566093ffa
Bob Merullo <bmerullo85@gmail.com>
2022-08-17 15:10:56 -0600

    added img upload to server

--- server/routes/project.routes.js
+++ server/routes/project.routes.js
@@ -1,9 +1,15 @@
 const projectsController = require("../controllers/projects.controller");
 const { authenticate } = require("../config/jwt.config");
+const multer = require("multer");
+const upload = require("../middleware/upload");
 
 module.exports = (app) => {
   app.get("/api/projects", projectsController.findAllProjects);
-  app.post("/api/projects", projectsController.createNewProject);
+  app.post(
+    "/api/projects",
+    upload.single("projectImage"),
+    projectsController.createNewProject
+  );
   app.get(
     "/api/user/projects/:userId",
     projectsController.findAllProjectsByUser
