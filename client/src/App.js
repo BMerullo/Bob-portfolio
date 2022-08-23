79c95d4ec27ff7bb9159c621ad2e6e0795e9e0e1
Bob Merullo <bmerullo85@gmail.com>
2022-08-22 19:48:29 -0600

    login finished

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Web from "./pages/Web";
 import Projects from "./pages/Projects";
 import Dropdown from "./components/Dropdown";
 import React, { useState } from "react";
-import Admin from "./pages/Admin";
+import AdminLogin from "./pages/AdminLogin";
 import ProjectForm from "./pages/ProjectForm";
 
 function App() {
@@ -20,8 +20,11 @@ function App() {
         <Route path="/web" element={<Web title={title} />} />
         <Route path="/projects" element={<Projects title={title} />} />
         <Route path="/test" element={<Dropdown />} />
-        <Route path="/admin" element={<Admin title={title} />} />
-        <Route path="/project-form" element={<ProjectForm title={title} />} />
+        <Route path="/admn-login" element={<AdminLogin title={title} />} />
+        <Route
+          path="/project-form/:id"
+          element={<ProjectForm title={title} />}
+        />
       </Routes>
     </div>
   );
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react";
+import Layout from "../components/Layout";
 
 const Projects = (props) => {
   return (
@@ -10,69 +10,14 @@ const Projects = (props) => {
             <h1 className="page-title">Projects</h1>
             <h5 className="eyebrow">A collection of my work</h5>
           </div>
-          <div className="project-container">
-            <div className="project-card">
-              <div className="project-img-container" id="starwars"></div>
-              <h3 style={cardStyle}>Starwars API</h3>
-              <a
-                className="project-link"
-                href="https://github.com/BMerullo/star-wars-api"
-                style={cardStyle}
-              >
-                source code
-              </a>
-            </div>
-            <div className="project-card">
-              <div className="project-img-container"></div>
-              <h3 style={cardStyle}>Project title</h3>
-              <a href="#" style={cardStyle}>
-                source code
-              </a>
-            </div>
-            <div className="project-card">
-              <div className="project-img-container"></div>
-              <h3 style={cardStyle}>Project title</h3>
-              <a href="#" style={cardStyle}>
-                source code
-              </a>
-            </div>
-            <div className="project-card">
-              <div className="project-img-container"></div>
-              <h3 style={cardStyle}>Project title</h3>
-              <a href="#" style={cardStyle}>
-                source code
-              </a>
-            </div>
-            <div className="project-card">
-              <div className="project-img-container"></div>
-              <h3 style={cardStyle}>Project title</h3>
-              <a href="#" style={cardStyle}>
-                source code
-              </a>
-            </div>
-            <div className="project-card">
-              <div className="project-img-container"></div>
-              <h3 style={cardStyle}>Project title</h3>
-              <a href="#" style={cardStyle}>
-                source code
-              </a>
-            </div>
-            <div className="project-card">
-              <div className="project-img-container"></div>
-              <h3 style={cardStyle}>Project title</h3>
-              <a href="#" style={cardStyle}>
-                source code
-              </a>
-            </div>
-          </div>
         </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
 
 const cardStyle = {
-  padding: '15px',
-}
+  padding: "15px",
+};
